67fc46ddd9767557cf7e585e8a8b18bcb328935d
MinasMazar <>
2024-05-25 12:23:06 +0200

    [wip] improve codeblock rendering

--- lib/apollo/gemini/gmi.ex
+++ lib/apollo/gemini/gmi.ex
@@ -2,41 +2,43 @@ defmodule Apollo.Gemini.Gmi do
   defstruct [uri: nil, lines: [], error: nil]
 
   def parse(chunks, context) when is_list(chunks) do
+    context = context
+    |> Map.put(:codeblock, false)
+
     {lines, context} = for line <- chunks, reduce: {[], context} do
       {lines, context} ->
 	{line, context} = parse(line, context)
-	{lines ++ [line], context}
+	{lines ++ [line], post_line_parse(context)}
     end
 
     lines
   end
 require Logger
   def parse(line, context) when is_binary(line) do
-    line = case String.slice(line, 0, 3) do
-	     "#" <> _ -> heading_to_html(line)
-	     "=> " -> anchor_to_html(line, context)
-	     "```" -> toggle_code_block(context)
-	     ">" <> _ -> quote_to_html(line)
-	     "" -> {:line, :empty}
-	     _ -> {:line, line}
-	   end
-    {line, context}
+    case String.slice(line, 0, 3) do
+      "#" <> _ -> line_to_html(:heading, line, context)
+      "=> " -> line_to_html(:anchor, line, context)
+      "```" -> line_to_html(:code, line, context)
+      ">" <> _ -> line_to_html(:quote, line, context)
+      "" -> line_to_html(:empty, line, context)
+      _ -> line_to_html(:line, line, context)
+    end
   end
 
-  defp heading_to_html(line) do
+  defp line_to_html(:heading, line, context = %{codeblock: false}) do
     with [_, sharps, heading] <- Regex.run(~r[(#+)\s(.+)], line) do
       head_level = Enum.min([1, Enum.max([String.length(sharps), 6])])
       font_size = case head_level do
 	h when h >= 1 and h <= 3 -> "text-2xl"
 	h when h > 3 -> "text-xl"
       end
-      {:heading, head_level, heading}
+      {{:heading, head_level, heading}, context}
     else
-      _ -> {:error, line}
+      _ -> {{:error, line}, context}
     end
   end
 
-  defp anchor_to_html(line, context) do
+  defp line_to_html(:anchor, line, context = %{codeblock: false}) do
     result = with [_, url, title] <- Regex.run(~r[^=>\s(.+?)\s(.+)], line) do
 	       {url, title}
 	     else
@@ -44,22 +46,36 @@ require Logger
 	     end
     with {url, title} <- result,
 	 {:ok, uri} <- URI.new(url) do
-      {:anchor, uri, title}
+      {{:anchor, uri, title}, context}
     else
-      _ -> {:anchor, :error, line}
+      _ -> {{:anchor, :error, line}, context}
     end
   end
 
-  defp quote_to_html(line) do
+  defp line_to_html(:quote, line, context = %{codeblock: false}) do
     with [_, block] <- Regex.run(~r[>\s(.+)], line) do
-      {:quote, block}
+      {{:quote, block}, context}
     end
   end
 
-  defp toggle_code_block(context) do
-    context = context
-    |> Map.put_new(:codeblock, false)
 
-    {:code, !context.codeblock}
+  defp line_to_html(:empty, line, context= %{codeblock: false}) do
+    {{:line, ""}, context}
+  end
+
+  defp line_to_html(:line, line, context= %{codeblock: false}) do
+    {{:line, line}, context}
+  end
+
+  defp line_to_html(:code, line, context = %{codeblock: flag}) do
+    {{:code_boundary, !flag}, %{context | codeblock: !flag}}
+  end
+
+  defp line_to_html(type, line, context = %{codeblock: true}) do
+    {{:code, line}, context}
+  end
+
+  defp post_line_parse(context) do
+    context
   end
 end
--- lib/apollo_web/components/gemini_components.ex
+++ lib/apollo_web/components/gemini_components.ex
@@ -9,7 +9,7 @@ defmodule ApolloWeb.GeminiComponents do
 
   def gmi(assigns) do
     ~H"""
-    <p :for={line <- @document.lines} class="py-1">
+    <p :for={line <- @document.lines}>
       <%#= inspect line %>
       <%= raw(line_to_html(line, @document)) %>
     </p>
@@ -46,21 +46,25 @@ defmodule ApolloWeb.GeminiComponents do
     # {:ok, uri} = URI.new(%URI{path: "/", query: query})
     # URI.to_string(uri)
     case ApolloWeb.proxy_link(url, gmi) do
-      {:gopher, apollo_url} -> "<a href=\"#{apollo_url}\">🚜 #{title}</a>"
-      {:gemini, _apollo_url} -> "<span class=\"cursor-pointer\" phx-click=\"navigate\" phx-value-url=\"#{url}\">🚀 #{title}</span>"
-      {_, url} -> "<a href=\"#{url}\" target=\"_blank\">🌐 #{title}</a>"
-      :error -> "<span>ERROR</span>"
+      {:gopher, apollo_url} -> "<a class=\"py-0.5\" href=\"#{apollo_url}\">🚜 #{title}</a>"
+      {:gemini, _apollo_url} -> "<span class=\"py-0.5 cursor-pointer\" phx-click=\"navigate\" phx-value-url=\"#{url}\">🚀 #{title}</span>"
+      {_, url} -> "<a class=\"py-0.5\" href=\"#{url}\" target=\"_blank\">🌐 #{title}</a>"
+      :error -> "<span class=\"py-0.5\">ERROR</span>"
     end
   end
 
-  defp line_to_html({:code, flag}, _gmi) do
-    if flag, do: "<code>", else: "</code>"
+  defp line_to_html({:code_boundary, flag}, _gmi) do
+    if flag, do: "<code class=\"py-2\">", else: "</code>"
+  end
+
+  defp line_to_html({:code, line}, _gmi) do
+    "<p class=\"-my-1.5\">#{line}</p>"
   end
 
   defp line_to_html({:quote, line}, _gmi) do
-    "<blockquote><tt>#{line}</tt></blockquote>"
+    "<blockquote class=\"py-2\"><tt>#{line}</tt></blockquote>"
   end
 
   defp line_to_html({:line, :empty}, _gmi), do: "<br>"
-  defp line_to_html({:line, line}, _gmi), do: "<span>#{line}</span>"
+  defp line_to_html({:line, line}, _gmi), do: "<p class=\"py-0.5\">#{line}</p>"
 end
