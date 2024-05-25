4da090128bc68dd5dda2c095f027a680ba352ea9
MinasMazar <>
2024-05-25 12:23:06 +0200

    Reorganize binstubs

---
+++ bin/rel.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+SECRET_KEY_BASE=p6mNyV2NyvZOBbWq/ox7cQsj2wgWDxkPlkRIArNbIn8WuRwmxpOlFMiO7yySWt2F DATABASE_URL=ecto://postgres:postgres@localhost/apollo _build/prod/rel/apollo/bin/server
