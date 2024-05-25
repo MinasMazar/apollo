4a0ff578112f355906bb9427ee470ede2b49089d
MinasMazar <>
2024-05-25 12:23:06 +0200

    Add useful binstub

---
+++ .iex.exs
@@ -0,0 +1,13 @@
+alias Apollo, as: A
+alias A.Gemini, as: G
+alias G.Api, as: Api
+
+defmodule Apollo.IEx.Helpers do
+  @test_url "gemini://geminiprotocol.net/docs/tech-overview.gmi"
+  def test_url, do: @test_url
+
+  def b(url \\ test_url()), do: G.body(url, :from_api)
+  def d(url \\ test_url()), do: G.to_gmi(url)
+end
+
+alias A.IEx.Helpers, as: H
