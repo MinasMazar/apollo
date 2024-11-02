839c9905eefd0c8a8bc6b40545b153c126a75f2b
MinasMazar <5578468+MinasMazar@users.noreply.github.com>
2024-11-02 13:48:37 +0100

    Configure check_origin for prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -58,6 +58,7 @@ if config_env() == :prod do
 
   config :apollo, ApolloWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    check_origin: ["//localhost", "//#{host}", "//apollo.minasmazar.org"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
